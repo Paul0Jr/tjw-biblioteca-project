96f313c315f15ab04db5b219da3a620b2556e3e6
Paulo Júnior <pjunior954@gmail.com>
2026-06-15 13:51:59 -0300

    update repository

--- livro-ms/src/main/java/com/biblioteca/livroms/repository/LivroRepository.java
+++ livro-ms/src/main/java/com/biblioteca/livroms/repository/LivroRepository.java
@@ -1,16 +1,14 @@
 package com.biblioteca.livroms.repository;
 
 import com.biblioteca.livroms.entity.Livro;
-
+import java.util.List;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LivroRepository extends JpaRepository<Livro, Long> {
     boolean existsById(Long id);
-    boolean existBytitulo(String titulo);
     Optional<Livro> findByTitulo(String titulo);
-
+    List<Livro> findByDisponivel(Boolean disponivel);
 }
