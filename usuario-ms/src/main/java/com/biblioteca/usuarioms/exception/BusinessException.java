ddd1436688888b962194ee3029a27f05af4066f0
jppn123 <jpiluca11@gmail.com>
2026-06-18 17:51:30 -0300

    adicionando microsserviço de usuários

---
+++ usuario-ms/src/main/java/com/biblioteca/usuarioms/exception/BusinessException.java
@@ -0,0 +1,7 @@
+package com.biblioteca.usuarioms.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
